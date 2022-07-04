daa31b21d5f545b7dfd736d3c71a4c3cf1269728
Phrit Nakgate <boss5500216@gmail.com>
2022-07-04 14:35:57 +0700

    Add files via upload

---
+++ Javascript Studies/2_array.js
@@ -0,0 +1,13 @@
+/*Array*/
+//1st way to create an array
+let myArray = new Array();
+myArray[0] = "Eiei";
+myArray[1] = "Hello";
+myArray[2] = "World";
+console.log(myArray);
+
+//2nd way to create an array
+let days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+console.log(days);
+
+console.log("วันแรกของสัปดาห์คือ",days[0]);
